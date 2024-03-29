05fab45af98940108da39b1d9fc601f3dafe2100
hkrain <hkrain@uiowa.edu>
2024-03-29 15:59:06 -0500

    initial commit

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ app.include_router(coupons_router)
 
 @app.get("/")
 async def view_index():
-    return FileResponse("./FrontEnd/index.html")
+    return FileResponse("./FrontEnd/couponfinder/index.html")
 
-app.mount("/", StaticFiles(directory="FrontEnd"), name="static")
+# Secret Menu Page
+@app.get("/dealscreetmenu/")
+async def view_secrets():
+    return FileResponse("./FrontEnd/secretmenu/index.html")
+
+# User page 
+@app.get("/users/")
+async def view_secrets():
+    return FileResponse("./FrontEnd/users/index.html")
+
+from fastapi.responses import FileResponse
+
+@app.get("/register")
+async def get_register():
+    return FileResponse("./FrontEnd/register/index.html")
+
+
+app.mount("/", StaticFiles(directory="FrontEnd/couponfinder"), name="coupons")
+app.mount("/dealscreetmenu/", StaticFiles(directory="FrontEnd/secretmenu"), name = "secret")
+app.mount("/users/", StaticFiles(directory="FrontEnd/users"), name = "users")
