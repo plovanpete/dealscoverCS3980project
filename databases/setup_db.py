7d0c5ad75467275b7c205633596a9976b7e3f030
hkrain <hkrain@uiowa.edu>
2024-04-19 12:22:44 -0500

    asdf

--- databases/setup_db.py
+++ databases/setup_db.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 import bcrypt
 from databases.db import get_db
+from pymongo.errors import PyMongoError
+
 
 db = get_db()
 client = MongoClient('mongodb+srv://admin:dealscover1@dealscovercluster.bxpq8ph.mongodb.net/')
@@ -9,18 +11,19 @@ db = client.userDB # `user_database` is the name of the database
 
 
 def add_user(username, password, email, admin):
-    users_collection = db.users  # `users` is the name of the collection
-
-    # Hashing the password before storing it
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-    # Creating a new user document
-    user_document = {
-        "username": username,
-        "password": hashed_password,
-        "email": email,
-        "admin": 'Y' if admin else 'N'
-    }
+    try:
+        users_collection = db.users
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        user_document = {
+            "username": username,
+            "password": hashed_password,
+            "email": email,
+            "admin": 'Y' if admin else 'N'
+        }
+        result = users_collection.insert_one(user_document)
+        print(f'Added user {username} with _id: {result.inserted_id}')
+    except PyMongoError as e:
+        print(f"An error occurred: {e}")
 
     # Inserting the user document into the collection
     result = users_collection.insert_one(user_document)
